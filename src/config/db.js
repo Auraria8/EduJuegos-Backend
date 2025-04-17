c4f5031581bf9efffa79bbaae9b794d0f230f862
Allopimain <fabian_acruz@soy.sena.edu.co>
2025-04-17 10:47:29 -0500

    ✨ Fix: Corregido nombre de variable de entorno para la contraseña de la base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, // 💡 si lo tienes definido en .env
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
