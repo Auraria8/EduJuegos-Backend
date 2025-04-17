43a42797674463b0c8e9924e8b7cb378303e71fd
Allopimain <fabian_acruz@soy.sena.edu.co>
2025-04-16 19:21:16 -0500

    Arreglada referencia a comentario en comentarioController

--- src/controllers/comentarioEstudianteController.js
+++ src/controllers/comentarioEstudianteController.js
@@ -3,7 +3,7 @@ const comentario = require('../models/comentario');
 exports.verComentariosEstudiante = (req, res) => {
     const id_estudiante = req.user.id;
 
-    Comentario.obtenerPorEstudiante(id_estudiante, (err, results) => {
+    comentario.obtenerPorEstudiante(id_estudiante, (err, results) => {
         if (err) {
             return res.status(500).json({ mensaje: 'Error al obtener comentarios' });
         }
@@ -11,3 +11,4 @@ exports.verComentariosEstudiante = (req, res) => {
         res.json(results);
     });
 };
+
